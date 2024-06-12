03812b32574fc92ec91cd79bffa0a0bd2e7bd293
mthierman <mthierman@gmail.com>
2024-06-12 11:21:03 -0600

    Change JUCE branch from juce8 to master

--- packages/juce.cmake
+++ packages/juce.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     juce
     GIT_REPOSITORY "https://github.com/juce-framework/JUCE.git"
-    GIT_TAG juce8
+    GIT_TAG master
     GIT_SHALLOW ON
     )
 
