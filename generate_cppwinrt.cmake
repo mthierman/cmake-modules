30bee92a25ae8d765f35f01f03f1919312b2c596
mthierman <mthierman@gmail.com>
2024-07-09 23:32:56 -0600

    Remove more stuff

--- generate_cppwinrt.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Generating C++/WinRT with WebView2...")
-execute_process(
-    COMMAND
-        cppwinrt -input "${webview2_SOURCE_DIR}/lib/Microsoft.Web.WebView2.Core.winmd" sdk -output
-        "${cppwinrt_SOURCE_DIR}/build/native/include"
-    WORKING_DIRECTORY "${cppwinrt_SOURCE_DIR}/bin"
-    )
--- vc_redist.cmake
+++
@@ -1,5 +0,0 @@
-file(
-    DOWNLOAD
-    "https://aka.ms/vs/17/release/vc_redist.x64.exe"
-    "${CMAKE_BINARY_DIR}/vc_redist.x64.exe"
-    )
