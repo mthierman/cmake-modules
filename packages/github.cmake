fce2dccb8ebab141714b7e60bd7e0feac5a0b3c6
mthierman <mthierman@gmail.com>
2024-07-09 23:23:47 -0600

    Add json to github

--- packages/github.cmake
+++ packages/github.cmake
@@ -36,3 +36,38 @@ function(
                 common::flags_deps
         )
 endfunction()
+
+function(
+    install_json
+    version
+    )
+    FetchContent_Declare(
+        json URL "https://github.com/nlohmann/json/releases/download/v${version}/include.zip"
+        )
+
+    FetchContent_MakeAvailable(json)
+
+    add_library(
+        json
+        INTERFACE
+        )
+
+    add_library(
+        nlohmann::json
+        ALIAS
+        json
+        )
+
+    target_sources(
+        json
+        PUBLIC FILE_SET
+               HEADERS
+               BASE_DIRS
+               "${json_SOURCE_DIR}/single_include/nlohmann"
+               FILES
+               "${json_SOURCE_DIR}/single_include/nlohmann/json.hpp"
+               "${json_SOURCE_DIR}/single_include/nlohmann/json_fwd.hpp"
+        )
+
+    target_compile_definitions(json INTERFACE NLOHMANN_JSON_NAMESPACE_NO_VERSION=1)
+endfunction()
