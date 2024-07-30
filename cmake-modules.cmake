5473aa0117229479b88725c0c32e3ea396027bd5
mthierman <mthierman@gmail.com>
2024-07-30 01:50:53 -0600

    Don't log progress

--- cmake-modules.cmake
+++ cmake-modules.cmake
@@ -170,6 +170,7 @@ function(fetch_wil)
     FetchContent_Declare(
         wil
         URL "https://www.nuget.org/api/v2/package/Microsoft.Windows.ImplementationLibrary/${FETCH_VERSION}"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(wil)
@@ -201,6 +202,7 @@ function(fetch_cppwinrt)
     FetchContent_Declare(
         cppwinrt
         URL "https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/${FETCH_VERSION}"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(cppwinrt)
@@ -236,6 +238,7 @@ function(fetch_webview2)
 
     FetchContent_Declare(
         webview2 URL "https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2/${FETCH_VERSION}"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(webview2)
@@ -290,6 +293,7 @@ function(fetch_ada)
     FetchContent_Declare(
         ada
         URL "https://github.com/ada-url/ada/releases/download/v${FETCH_VERSION}/singleheader.zip"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(ada)
@@ -331,6 +335,7 @@ function(fetch_json)
 
     FetchContent_Declare(
         json URL "https://github.com/nlohmann/json/releases/download/v${FETCH_VERSION}/include.zip"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(json)
@@ -369,6 +374,7 @@ function(fetch_sqlite)
 
     FetchContent_Declare(
         sqlite URL "https://www.sqlite.org/2024/sqlite-amalgamation-${FETCH_VERSION}.zip"
+        DOWNLOAD_NO_PROGRESS
         )
 
     FetchContent_MakeAvailable(sqlite)
