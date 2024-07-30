32f4d6728740d47f816527d3e0a1276fbfc169c9
mthierman <mthierman@gmail.com>
2024-07-30 10:58:20 -0600

    Add glow/clap/clap-helpers fetchers

--- cmake-modules.cmake
+++ cmake-modules.cmake
@@ -500,3 +500,105 @@ function(make_release_info)
         OUTPUT_FILE "${NOTES_PATH}/release_notes"
         )
 endfunction()
+
+function(fetch_glow)
+    set(args VERSION)
+    cmake_parse_arguments(
+        FETCH
+        ""
+        "${args}"
+        ""
+        ${ARGN}
+        )
+
+    FetchContent_Declare(
+        glow
+        GIT_REPOSITORY "https://github.com/mthierman/Glow.git"
+        GIT_TAG ${FETCH_VERSION}
+        GIT_SHALLOW ON
+        )
+
+    FetchContent_MakeAvailable(glow)
+endfunction()
+
+function(fetch_clap)
+    set(args VERSION)
+    cmake_parse_arguments(
+        FETCH
+        ""
+        "${args}"
+        ""
+        ${ARGN}
+        )
+
+    FetchContent_Declare(
+        clap
+        GIT_REPOSITORY "https://github.com/free-audio/clap.git"
+        GIT_TAG ${FETCH_VERSION}
+        GIT_SHALLOW ON
+        SOURCE_SUBDIR
+        "NULL"
+        )
+
+    FetchContent_MakeAvailable(clap)
+
+    add_library(
+        clap
+        INTERFACE
+        )
+
+    add_library(
+        free-audio::clap
+        ALIAS
+        clap
+        )
+
+    target_sources(
+        clap
+        INTERFACE FILE_SET
+                  HEADERS
+                  BASE_DIRS
+                  "${clap_SOURCE_DIR}/include"
+        )
+endfunction()
+
+function(fetch_clap_helpers)
+    set(args VERSION)
+    cmake_parse_arguments(
+        FETCH
+        ""
+        "${args}"
+        ""
+        ${ARGN}
+        )
+
+    FetchContent_Declare(
+        clap-helpers
+        GIT_REPOSITORY "https://github.com/free-audio/clap-helpers.git"
+        GIT_TAG ${FETCH_VERSION}
+        GIT_SHALLOW ON
+        SOURCE_SUBDIR
+        "NULL"
+        )
+
+    FetchContent_MakeAvailable(clap-helpers)
+
+    add_library(
+        clap-helpers
+        INTERFACE
+        )
+
+    add_library(
+        free-audio::clap-helpers
+        ALIAS
+        clap-helpers
+        )
+
+    target_sources(
+        clap-helpers
+        INTERFACE FILE_SET
+                  HEADERS
+                  BASE_DIRS
+                  "${clap-helpers_SOURCE_DIR}/include"
+        )
+endfunction()
