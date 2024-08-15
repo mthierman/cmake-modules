5ac76387152f8ab801e97f5bc1eb6fbfe627bedb
mthierman <mthierman@gmail.com>
2024-08-15 00:39:42 -0600

    Fix clap-wrapper

--- fetch/fetch_clap_wrapper.cmake
+++ fetch/fetch_clap_wrapper.cmake
@@ -8,14 +8,26 @@ function(fetch_clap_wrapper)
         ${ARGN}
         )
 
+    if(TARGET
+       clap-wrapper
+        )
+        message(WARNING "fetch_clap_wrapper already called, ignoring")
+        return()
+    endif()
+
     include(FetchContent)
 
     FetchContent_Declare(
         clap-wrapper
-        GIT_REPOSITORY "https://github.com/free-audio/clap-wrapper.git"
-        GIT_TAG ${FETCH_VERSION}
-        GIT_SHALLOW ON
+        URL "https://github.com/free-audio/clap-helpers/archive/refs/heads/${FETCH_VERSION}.zip"
+        DOWNLOAD_NO_PROGRESS TRUE
         )
 
     FetchContent_MakeAvailable(clap-wrapper)
+
+    add_library(
+        free-audio::clap-wrapper
+        ALIAS
+        clap-wrapper
+        )
 endfunction()
