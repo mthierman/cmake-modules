c11fb02191b67436a5c430bf1a4ba767040f0e75
mthierman <mthierman@gmail.com>
2024-09-16 18:01:31 -0600

    Update RtMidi

--- fetch/fetch_rtmidi.cmake
+++ fetch/fetch_rtmidi.cmake
@@ -19,30 +19,30 @@ function(fetch_rtmidi)
     include(FetchContent)
 
     FetchContent_Declare(
-        rtmidi
+        thestk_rtmidi
         URL "https://github.com/thestk/rtmidi/archive/refs/heads/${FETCH_VERSION}.zip"
         DOWNLOAD_NO_PROGRESS TRUE
         SOURCE_SUBDIR
         "NULL"
         )
 
-    FetchContent_MakeAvailable(rtmidi)
+    FetchContent_MakeAvailable(thestk_rtmidi)
 
-    add_library(rtmidi)
+    add_library(thestk_rtmidi)
 
     add_library(
         thestk::rtmidi
         ALIAS
-        rtmidi
+        thestk_rtmidi
         )
 
-    file(COPY "${rtmidi_SOURCE_DIR}/RtMidi.h"
+    file(COPY "${thestk_rtmidi_SOURCE_DIR}/RtMidi.h"
          DESTINATION "${CMAKE_BINARY_DIR}/include/thestk/rtmidi"
         )
 
     target_sources(
-        rtmidi
-        PRIVATE "${rtmidi_SOURCE_DIR}/RtMidi.cpp"
+        thestk_rtmidi
+        PRIVATE "${thestk_rtmidi_SOURCE_DIR}/RtMidi.cpp"
         PUBLIC FILE_SET
                HEADERS
                BASE_DIRS
@@ -50,7 +50,7 @@ function(fetch_rtmidi)
         )
 
     target_link_libraries(
-        rtmidi
+        thestk_rtmidi
         PRIVATE common::compile_features
                 common::compile_definitions
                 common::compile_options_no_warnings
