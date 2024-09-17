5262ce62839433702ad0882425c6bdc7b9b6bb49
mthierman <mthierman@gmail.com>
2024-09-16 18:01:06 -0600

    Update RtAudio

--- fetch/fetch_rtaudio.cmake
+++ fetch/fetch_rtaudio.cmake
@@ -19,30 +19,30 @@ function(fetch_rtaudio)
     include(FetchContent)
 
     FetchContent_Declare(
-        rtaudio
+        thestk_rtaudio
         URL "https://github.com/thestk/rtaudio/archive/refs/heads/${FETCH_VERSION}.zip"
         DOWNLOAD_NO_PROGRESS TRUE
         SOURCE_SUBDIR
         "NULL"
         )
 
-    FetchContent_MakeAvailable(rtaudio)
+    FetchContent_MakeAvailable(thestk_rtaudio)
 
-    add_library(rtaudio)
+    add_library(thestk_rtaudio)
 
     add_library(
         thestk::rtaudio
         ALIAS
-        rtaudio
+        thestk_rtaudio
         )
 
-    file(COPY "${rtaudio_SOURCE_DIR}/RtAudio.h"
+    file(COPY "${thestk_rtaudio_SOURCE_DIR}/RtAudio.h"
          DESTINATION "${CMAKE_BINARY_DIR}/include/thestk/rtaudio"
         )
 
     target_sources(
-        rtaudio
-        PRIVATE "${rtaudio_SOURCE_DIR}/RtAudio.cpp"
+        thestk_rtaudio
+        PRIVATE "${thestk_rtaudio_SOURCE_DIR}/RtAudio.cpp"
         PUBLIC FILE_SET
                HEADERS
                BASE_DIRS
@@ -50,7 +50,7 @@ function(fetch_rtaudio)
         )
 
     target_link_libraries(
-        rtaudio
+        thestk_rtaudio
         PRIVATE common::compile_features
                 common::compile_definitions
                 common::compile_options_no_warnings
