48d673bfc1a1c071f31fcb95d5143092f2c1780d
mthierman <mthierman@gmail.com>
2024-09-16 18:00:30 -0600

    Update JUCE

--- fetch/fetch_juce.cmake
+++ fetch/fetch_juce.cmake
@@ -19,9 +19,9 @@ function(fetch_juce)
     include(FetchContent)
 
     FetchContent_Declare(
-        juce URL "https://github.com/juce-framework/JUCE/archive/refs/heads/${FETCH_VERSION}.zip"
+        juce_juce URL "https://github.com/juce-framework/JUCE/archive/refs/heads/${FETCH_VERSION}.zip"
         DOWNLOAD_NO_PROGRESS TRUE
         )
 
-    FetchContent_MakeAvailable(juce)
+    FetchContent_MakeAvailable(juce_juce)
 endfunction()
