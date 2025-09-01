e8e3214edf0fdbadfbea5dab300c3c738a5e5aa7
mthierman <mthierman@gmail.com>
2025-09-01 16:36:14 -0600

    Improve find_innosetup

--- utility/find_innosetup.cmake
+++ utility/find_innosetup.cmake
@@ -3,9 +3,9 @@ function(find_innosetup)
         INNOSETUP_COMPILER_EXE
         iscc
         )
-    if(INNOSETUP_COMPILER_EXE)
-        message(STATUS "Using Inno Setup from ${INNOSETUP_COMPILER_EXE}")
-    else()
+    if(NOT
+       INNOSETUP_COMPILER_EXE
+        )
         file(
             DOWNLOAD
             "https://files.jrsoftware.org/is/6/innosetup-6.5.1.exe"
@@ -13,8 +13,14 @@ function(find_innosetup)
             EXPECTED_HASH SHA256=3622FFDAD7B2534239370099149C33ADB85B90054799D901CB8EC844DF7A0E41
             )
         execute_process(
-            COMMAND "${CMAKE_BINARY_DIR}/innosetup-6.5.1.exe" /VERYSILENT /CURRENTUSER /DIR=iscc
-            RESULT_VARIABLE install_result
+            COMMAND
+                "${CMAKE_BINARY_DIR}/innosetup-6.5.1.exe" /VERYSILENT /CURRENTUSER
+                /DIR=innosetup-6.5.1
+            )
+        find_program(
+            INNOSETUP_COMPILER_EXE
+            iscc
+            PATHS ${CMAKE_BINARY_DIR}/innosetup-6.5.1
             )
     endif()
 endfunction()
